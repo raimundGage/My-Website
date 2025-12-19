af4bf49018c0182754759bea4c58351e9d15d55c
raimundGage <raimundgage@outlook.com>
2025-12-19 14:48:49 +0800

    Learning dynamic values & multiple components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
 import { useState } from 'react'
+import Navbar from './components/Navbar';
 
 function App() {
+  const greet = "good morniiiiing";
+  const visits = 30;
+  const wikiLink = "http://www.wikipedia.com";
+
   return (
     <>
-      <div className='text-[#e3c637]'>good morniiiiing</div>
+      <Navbar />
+      
+      <div className='text-[#e3c637]'>{ greet }</div>
+      <div className='text-[#e53c4a] text-sm font-mono'>There are { visits } people who visited this.</div>
+
+      <a href={ wikiLink }>Wiki!</a>
     </>
   )
 }
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="">
+            useful navigation!
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
