a9dcfb71be8e9d75832db5619978dc1415a0627a
raimundGage <raimundgage@outlook.com>
2025-12-27 10:34:02 +0800

    cleaning the code format!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,50 @@
-import { Button } from './pieces/Button';
-import { Marquee } from './pieces/Marquee';
-import { RetroWindow } from './components/RetroWindow';
-import { Window} from './components/Window';
+import { Button } from "./pieces/Button";
+import { Marquee } from "./pieces/Marquee";
+import { RetroWindow } from "./components/RetroWindow";
+import { Window } from "./components/Window";
 
 function App() {
   const favorites = [
-    {icon: "🐯", name: "Grantly Bell",    from: "Knights College"            },
-    {icon: "🐺", name: "Albert",          from: "Intermarché - Conte de Noël"},
-    {icon: "🐲", name: "Cold Snapdragon", from: "Plants VS Zombies 2"        },
+    { icon: "🐯", name: "Grantly Bell", from: "Knights College" },
+    { icon: "🐺", name: "Albert", from: "Intermarché - Conte de Noël" },
+    { icon: "🐲", name: "Cold Snapdragon", from: "Plants VS Zombies 2" },
   ];
 
   return (
     <article className="App">
-
       <section>
         {/* Scrolling Marquee Of My Faves */}
         <div>
           {favorites.map((favs) => {
-            return <Marquee key={favs.name} icon={favs.icon} name={favs.name} from={favs.from} />
+            return (
+              <Marquee
+                key={favs.name}
+                icon={favs.icon}
+                name={favs.name}
+                from={favs.from}
+              />
+            );
           })}
         </div>
-        
+
         {/* Button */}
-        <Button buttonText={"mrowwww"}/>
+        <Button buttonText={"mrowwww"} />
       </section>
 
       {/* Load The Image Inside The Window Component */}
-      <RetroWindow title={"some_cats"} imageSource={"https://pbs.twimg.com/media/G803GDrb0AA6H-M?format=jpg&name=large"} />
-      
-      {/* Practicing To Replicate The Old Window */}
-      <div className='mt-3'>
-        <Window title={"Who Am I?"} content={"I'm Raimund Gage!"}/>
-      </div>
+      <RetroWindow
+        title={"some_cats"}
+        imageSource={
+          "https://pbs.twimg.com/media/G803GDrb0AA6H-M?format=jpg&name=large"
+        }
+      />
 
+      {/* Practicing To Replicate The Old Window */}
+      <div className="mt-3">
+        <Window title={"Who Am I?"} content={"I'm Raimund Gage!"} />
+      </div>
     </article>
-  )
+  );
 }
 
 export default App;
--- frontend/src/components/Window.jsx
+++ frontend/src/components/Window.jsx
@@ -1,27 +1,29 @@
 export const Window = ({ title, content }) => {
-    return (
-        <section>
-            
-            <div className="bg-win-grey p-1 max-w-2xl border-2 shadow-[1px_1px_0_0_#000000]
-                            border-l-win-light border-t-win-light border-r-win-dark border-b-win-dark">
-                
-                {/* Title */}
-                <div className="bg-win-blue px-2 py-1 mb-2 flex justify-between items-center">
-                    <span className="text-white">{title}</span>
-                    <div className="flex gap-1">
-                        <button className="px-1 text-xs leading-none font-bold bg-win-grey border border-l-white border-t-white border-r-black border-b-black">?</button>
-                        <button className="px-1 text-xs leading-none font-bold bg-win-grey border border-l-white border-t-white border-r-black border-b-black">-</button>
-                        <button className="px-1 text-xs leading-none font-bold bg-win-grey border border-l-white border-t-white border-r-black border-b-black">X</button>
-                    </div>
-                </div>
+  return (
+    <section>
+      <div
+        className="bg-win-grey p-1 max-w-2xl border-2 shadow-[1px_1px_0_0_#000000]
+                            border-l-win-light border-t-win-light border-r-win-dark border-b-win-dark"
+      >
+        {/* Title */}
+        <div className="bg-win-blue px-2 py-1 mb-2 flex justify-between items-center">
+          <span className="text-white">{title}</span>
+          <div className="flex gap-1">
+            <button className="px-1 text-xs leading-none font-bold bg-win-grey border border-l-white border-t-white border-r-black border-b-black">
+              ?
+            </button>
+            <button className="px-1 text-xs leading-none font-bold bg-win-grey border border-l-white border-t-white border-r-black border-b-black">
+              -
+            </button>
+            <button className="px-1 text-xs leading-none font-bold bg-win-grey border border-l-white border-t-white border-r-black border-b-black">
+              X
+            </button>
+          </div>
+        </div>
 
-                {/* Content */}
-                <div className="bg-win-light">
-                    {content}
-                </div>
-
-            </div>
-
-        </section>
-    );
+        {/* Content */}
+        <div className="bg-win-light">{content}</div>
+      </div>
+    </section>
+  );
 };
